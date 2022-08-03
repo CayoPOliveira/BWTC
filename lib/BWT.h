86e50cf7bb65968d7b222a77c7a38bd93d176204
CayoP <cayophellipe1@gmail.com>
2022-08-03 14:39:41 -0300

    Criando biblioteca para algoritmos da BWT, possui uma função para criar fazer a transformada e função para transformada inversa

---
+++ lib/BWT.h
@@ -0,0 +1,28 @@
+#ifndef BWT_H
+#define BWT_H
+
+#include "DC3.h"
+
+typedef uint_t str_t;
+
+/*
+  BWT_Construct() aplica a transformada de Burrows-Wheeller a um array e retorna o resultado;
+
+  @param *Str  String que será aplicada a transformada
+  @param *BWT  Buffer que guardará a transformada
+  @param N    Tamanho do array
+*/
+void BWT_Construct(str_t *Str, str_t *BWT, uint_t N);
+
+/*
+  BWT_Inverse_Construct() aplica a transformada inversa de Burrows-Wheeller para gerar a string geradora;
+
+  @param *BWT    String já aplicada a transformada
+  @param *Str    Buffer que guardará a string geradora
+  @param N      Tamanho do array
+  @param Sigma  Máximo valor do vetor (exemplo: maximo valor da ASCII - 256)
+
+*/
+void BWT_Inverse_Construct(str_t *BWT, str_t *Str, uint_t N, uint_t Sigma);
+
+#endif
---
+++ src/BWT.c
@@ -0,0 +1,44 @@
+#include "BWT.h"
+#include <stdlib.h>
+
+void BWT_Construct(str_t *Str, str_t *BWT, uint_t N)
+{
+  str_t *SA = (str_t *)malloc(N * sizeof(str_t));
+  for (uint_t i = 0; i < N; i++)
+    SA[i] = 0;
+  DC3(Str, N, SA, 255);
+  for (uint_t i = 0; i < N; i++)
+  {
+    uint_t sai = SA[i];
+    BWT[i] = (!sai ? (Str[N - 1]) : (Str[sai - 1]));
+  }
+  free(SA);
+}
+
+void BWT_Inverse_Construct(str_t *BWT, str_t *Str, uint_t N, uint_t Sigma)
+{
+  Sigma++;
+  uint_t *Count = (uint_t *)malloc(Sigma * sizeof(uint_t));
+  uint_t *LF = (uint_t *)malloc(Sigma * sizeof(uint_t));
+
+  for (uint_t i = 0; i < Sigma; i++)
+    Count[i] = 0;
+  for (uint_t i = 0; i < N; i++)
+    Count[BWT[i]]++;
+  for (uint_t i = 0, val = 0; i < Sigma; i++)
+  {
+    val += Count[i];
+    Count[i] = val - Count[i];
+  }
+  for (uint_t i = 0, j = 0; i < N; i++)
+  {
+    j = BWT[i];
+    LF[Count[j]] = i;
+    Count[j]++;
+  }
+  for (uint_t i = 0, j = LF[0]; i < N; i++)
+  {
+    j = LF[j];
+    Str[i] = BWT[j];
+  }
+}
